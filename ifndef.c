b519d4316c87dc1d1dd9273ae629a7065e25024c
erroll <37023967+erro11@users.noreply.github.com>
2018-06-04 01:51:53 +0300

    Update ifndef.c

--- ifndef.c
+++ ifndef.c
@@ -2,3 +2,49 @@
 #include "ifnded1.txt"
 #include "ifnded1.txt"
 #include "ifnded1.txt"
+
+
+result 
+gcc -E ifndef.c -o ifndef.i
+
+# 1 "ifndef.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 1 "ifndef.c"
+# 1 "ifnded1.txt" 1
+
+
+first
+
+
+
+every
+# 2 "ifndef.c" 2
+# 1 "ifnded1.txt" 1
+
+
+
+
+repeat
+
+every
+# 3 "ifndef.c" 2
+# 1 "ifnded1.txt" 1
+
+
+
+
+repeat
+
+every
+# 4 "ifndef.c" 2
+# 1 "ifnded1.txt" 1
+
+
+
+
+repeat
+
+every
+# 5 "ifndef.c" 2
+
