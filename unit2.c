0fed6c3b0d6a7d7e140c71fadd0f0c5ff28adbb6
erroll <37023967+erro11@users.noreply.github.com>
2018-06-04 01:14:45 +0300

    Create unit2.c

---
+++ unit2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>//предназначена для включения в программу текста файла из стандартного каталога
+//#include "stdio.h"//предназначена для включения в программу текста файла из любого каталога, а поиск начинается в текущем
+//#include "text.txt"
+//
+// #include "c:/users/lotus/desktop/c/unit1/unit2_1aC.txt"
+// #include "c:/users/lotus/desktop/c/unit1/unit2_1bC.txt"  если путь к директиве препроцессора не в рабочем каталоге
+
+
+#include "unit2_1aC.txt"
+#include "unit2_1bC.txt"
+
+  puts("a");
+  return 0;
+}
