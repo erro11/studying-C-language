345222e60579aa0eb5baf0908481a0cd2b7a288c
erroll <37023967+erro11@users.noreply.github.com>
2018-06-07 03:40:02 +0300

    Update truefalse.c

--- truefalse.c
+++ truefalse.c
@@ -9,7 +9,11 @@ int main()
   PRINTI(5==6);
   PRINTI(3<6);
   PRINTI(41<6);
-  PRINTI(5==5);\
+  PRINTI(5==5);
+  PRINTI(!2);
+  PRINTI(!(6<2));
+  PRINTI(!6<0);
+  PRINTI(!6<2);
   return 0;
 }
 
@@ -23,4 +27,7 @@ the value of 5==6=0
 the value of 3<6=1             
 the value of 41<6=0      
 the value of 5==5=1  
-
+the value of !3=0
+the value of !6<2=1
+the value of !6<0=0                 
+the value of !6<2=1 
